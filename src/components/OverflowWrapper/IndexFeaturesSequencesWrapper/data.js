c5451e1086ff2d8329653e47ad76057216ee825c
FocusSaad <saaooodis@gmail.com>
2025-02-09 00:40:41 +0300

    Update data.js

--- src/components/OverflowWrapper/IndexFeaturesSequencesWrapper/data.js
+++ src/components/OverflowWrapper/IndexFeaturesSequencesWrapper/data.js
@@ -124,6 +124,6 @@ export const IndexFeaturesSequence = [
         ],
         imgSrc: cardsOverlayImg,
         link: 'virtual-cards',
-        title: 'دفع مباشر بالبطاقة الائتمانية',
+        title: 'دفع فوري بالبطاقة الائتمانية',
     },
 ];
