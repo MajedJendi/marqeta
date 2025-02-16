d79092b860fa7b5b0672620154da4cc05889abb2
Majed Jendi <54814868+MajedJendi@users.noreply.github.com>
2025-02-16 15:43:05 -0700

    Update data.js

--- src/components/IndexPartners/IndexPartnersPartnersWrapper/data.js
+++ src/components/IndexPartners/IndexPartnersPartnersWrapper/data.js
@@ -3,7 +3,7 @@ import card2Img from '@assets/img/partners/card-2.png';
 import WUCardImg from '@assets/img/partners/WU_Card.svg';
 import partnersVideo3Img from '@assets/img/partners/partners-video-3.png';
 
-export const IndexPartnersPartnersBlockHeadTestimonialsItem = ['أكثر من 10 سنين من الخبرة في قطاعات تجارية وتقنية مختلفة', ' خبراء تقنيين عالميين', ' نمو سنوي عالي في عمليات الدفع عن طريق البطاقات الائتمانية '];
+export const IndexPartnersPartnersBlockHeadTestimonialsItem = ['أكثر من 10 سنين من الخبرة في قطاعات تجارية وتقنية مختلفة', '• خبراء تقنيين عالميين', '• نمو سنوي عالي في عمليات الدفع عن طريق البطاقات الائتمانية •'];
 export const IndexPartnersPartnersSliderItem = [
     {
         src: card1Img,
