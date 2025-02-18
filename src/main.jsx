459f10c642f7b9cd51219b9f80eb3d2e0307ab3a
Majed Jendi <54814868+MajedJendi@users.noreply.github.com>
2025-02-17 19:23:30 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@app/App';
+import ContactUs from '@/Pages/ContactUs';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import './index.css';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/contact-us",
+    element: <ContactUs/>,
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
 );
