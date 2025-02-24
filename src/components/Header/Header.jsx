a3b35cb253a4e7d0af2d9e426c70100729dccc25
Majed Jendi <54814868+MajedJendi@users.noreply.github.com>
2025-02-24 10:42:57 -0700

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -69,6 +69,11 @@ export function Header() {
                     </div>
                 </div>
             </div>
+
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet" />
+            
         </header>
     );
 }
